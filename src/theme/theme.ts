7db1cfa04642fd7ec3c68e5bb54396326d6741f1
romka <romul3003@gmail.com>
2022-07-04 15:50:16 +0300

    Styled components reserch

---
+++ src/theme/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme } from '@mui/material/styles'
+import { green, orange, cyan } from '@mui/material/colors'
+
+const TEST_COLOR_KEY = 500
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: cyan[TEST_COLOR_KEY],
+    },
+    secondary: {
+      main: green[TEST_COLOR_KEY],
+    },
+  },
+  status: {
+    danger: orange[TEST_COLOR_KEY],
+  },
+})
